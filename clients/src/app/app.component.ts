9c526d1de95e061ca53ac3130f49f3f9f6073d95
Momchil Palazov <momchil.palazov@gmail.com>
2024-03-12 19:41:51 +0100

    Making HTTP request in Angular

--- clients/src/app/app.component.ts
+++ clients/src/app/app.component.ts
@@ -1,19 +1,33 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-
-  
+export class AppComponent implements OnInit {
 
   title = 'Hello';
+  users: any;
+  constructor(private  http: HttpClient) {}  
 
-  refresshPage()
-  {
-    window.location.reload();
+  ngOnInit(): void {
+    
+    this.http.get('https://localhost:5001/api/users').subscribe( {
+      next: (response) => {
+        this.users = response;
+      },
+      error: (error) => {
+        console.log(error);
+      },
+      complete: () => {
+        console.log('complete');
+      }
+    });
+    
+    
   }
 
 
