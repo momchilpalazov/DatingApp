25a233c9e25c9da8eefdc62b8efcff38b0625571
Momchil Palazov <momchil.palazov@gmail.com>
2024-03-13 22:31:57 +0100

    Add https using mkcert in Angulra

--- API/Program.cs
+++ API/Program.cs
@@ -16,7 +16,7 @@ builder.Services.AddCors(
     {
         options.AddPolicy("CorsPolicy", policy =>
         {
-            policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200");
+            policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
 
         });
     }
--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2471b8c3f0cd80991970176cacf53a9f7690b2d6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e080c940b8c72d4c9d709263709f543b930149bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
