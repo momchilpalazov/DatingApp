8fcfb54cc64a3c35b9e6b5dbd4f85411120895af
Momchil Palazov <momchil.palazov@gmail.com>
2024-03-14 20:18:07 +0100

    Add extensions methods

--- API/Program.cs
+++ API/Program.cs
@@ -1,51 +1,22 @@
 using System.Text;
 using API.Data;
+using API.Extensions;
 using API.Interfaces;
 using API.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<DaitingAppDbContext>(options => 
-{
-    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")); 
-});
-
-builder.Services.AddCors(
-
-    options =>
-    {
-        options.AddPolicy("CorsPolicy", policy =>
-        {
-            policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
-
-        });
-    }
-);
-
-builder.Services.AddScoped<ITokenService,TokenService>();
-
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options =>
-    {
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuerSigningKey = true,
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["TokenKey"])),
-            ValidateIssuer = false,
-            ValidateAudience = false
-        };
-    });
-
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 //app.UseHttpsRedirection();
-app.UseCors("CorsPolicy");
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
 app.UseAuthentication();
 app.UseAuthorization();
