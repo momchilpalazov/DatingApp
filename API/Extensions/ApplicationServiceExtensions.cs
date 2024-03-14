045e23a52a04907dca6f0c254baf8c16a093afe5
Momchil Palazov <momchil.palazov@gmail.com>
2024-03-14 20:18:33 +0100

    Add extensions methods

---
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+using API.Data;
+using API.Interfaces;
+using API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+
+namespace API.Extensions;
+
+public static class ApplicationServiceExtensions
+{
+
+public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
+    {
+        services.AddDbContext<DaitingAppDbContext>(options => 
+        {
+            options.UseSqlite(config.GetConnectionString("DefaultConnection")); 
+        });
+
+        services.AddCors();
+        services.AddScoped<ITokenService,TokenService>();
+       
+        return services;      
+
+
+
+    }
+    
+}
--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf2885a251132a92f937f5396791398f4e01c425")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1bb0e8545a4253a495a64e7f9d3218a68dc04444")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
