38a7e38ee30e098119ca1067212b1abd765e2ef3
Momchil Palazov <momchil.palazov@gmail.com>
2024-03-14 16:34:14 +0100

    Using DTOs

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36e0dd2145f6bd280a450fef8362507b21c3636c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e792883ac8fc168229870edfebfaa8709633b33d")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
