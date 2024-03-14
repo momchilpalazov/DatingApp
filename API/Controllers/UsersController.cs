e792883ac8fc168229870edfebfaa8709633b33d
Momchil Palazov <momchil.palazov@gmail.com>
2024-03-14 15:07:54 +0100

    Creating bascontroller

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -4,9 +4,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
-[ApiController]
-[Route("api/[controller]")]
-public class UsersController : ControllerBase
+
+public class UsersController : BaseController
 {
     private readonly DaitingAppDbContext _context;
 
