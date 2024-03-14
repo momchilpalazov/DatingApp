36e0dd2145f6bd280a450fef8362507b21c3636c
Momchil Palazov <momchil.palazov@gmail.com>
2024-03-14 14:56:42 +0100

    Updating user entity

--- API/Data/Migrations/20240314134807_UserPasswordHash.Designer.cs
+++ API/Data/Migrations/20240314134807_UserPasswordHash.Designer.cs
@@ -2,6 +2,7 @@
 using API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace API.Data.Migrations
 {
     [DbContext(typeof(DaitingAppDbContext))]
-    partial class DaitingAppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240314134807_UserPasswordHash")]
+    partial class UserPasswordHash
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.2");
--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e080c940b8c72d4c9d709263709f543b930149bc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25a233c9e25c9da8eefdc62b8efcff38b0625571")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
