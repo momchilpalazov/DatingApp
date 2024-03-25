87ee0f5cd9616dc989becaddd863c770bdf61eb7
Momchil Palazov <momchil.palazov@gmail.com>
2024-03-25 15:01:34 +0100

    Changes

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bdace664b38511fcf3dfd299ae251559d8fafdb6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+699c1d3de66cabdc1ffb3c77b0f93a066d0cfda2")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
