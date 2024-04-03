423f6c66f15f82d7a375dd79bafe466b87b56a13
Momchil Palazov <momchil.palazov@gmail.com>
2024-04-03 18:47:09 +0200

    Adding guard Can Deactivate

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5295f056f26297fd7d32495b7954cb7fa12ea1e7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9e649e111cf4f08158036bdfa57f1d69be31d83")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
